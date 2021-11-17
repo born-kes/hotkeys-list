e39773c3ccabca5e54f51f9f75acb82b932a79b2
bornkes <born-kes@o2.pl>
2021-11-18 00:32:05 +0100

    add hotkeys in all editor text

--- hotkeys_data.php
+++ hotkeys_data.php
@@ -1,12 +1,16 @@
 <?php
 return ( [
+    'all editor text' => [
+        'select a word' => 'Ctrl + Shift + ->',
+        'jump a word' => 'Ctrl + ->'
+    ],
     'excel' => [
-        'Add Filter' => 'Ctrl + Shift + L'
+        'Add Filter' => 'Ctrl + Shift + L',
     ],
     'PhpStrom' => [
-       'Reformat code' => 'Ctrl + Alt + L'
+       'Reformat code' => 'Ctrl + Alt + L',
     ],
     'Netbeans' => [
-        'Reformat code' => 'Alt + Shift + F'
+        'Reformat code' => 'Alt + Shift + F',
     ],
 ]);
