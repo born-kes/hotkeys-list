a13c1d15b80513c6e954a2ff3b83bd13aa4ccaf7
bornkes <born-kes@o2.pl>
2021-11-21 23:02:47 +0100

    Add define more arrow
    don't trust string!
    define prepare to use in future

--- hotkeys_data.php
+++ hotkeys_data.php
@@ -3,6 +3,9 @@ if(!function_exists('t')){ function t($a){return $a;} }
 define('CTRL', t('Ctrl') );
 define('SHIFT', t('Shift') );
 define('DOWN_ARROW', t('↓') );
+define('UPWARDS_ARROW', t('↑') );
+define('LEFTWARDS_ARROW', t('←') );
+define('RIGHTWARDS_ARROW', t('→') );
 
 return ( [
     t('all editor text') => [
