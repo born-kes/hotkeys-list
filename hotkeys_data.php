fc0ab4ffadc00c52acbb446fae5b4df280c79f06
born-kes <born-kes@o2.pl>
2022-01-05 22:13:10 +0100

    add hotkey - Run debug session start in browser

--- hotkeys_data.php
+++ hotkeys_data.php
@@ -71,6 +71,7 @@ return ( [
 		t('Select in projects/files/favorites') => t('%s + %s + %s', [CTRL, SHIFT, '2']),
 		t('Select in projects/files/favorites') => t('%s + %s + %s', [CTRL, SHIFT, '3']),
 		
+		t('Run debuger') => t('%s + %s', [CTRL, 'F5']),
 		t('Run Unit test') => t('%s + %s', [CTRL, 'F6']),
 		t('Run Unit test') => t('%s + %s', [ALT, 'F6']),
 		t('Delete row') => t('%s + %s', [CTRL, 'e']),
