024b98131986641bb904ad021d6e4a5631b510da
born-kes <born-kes@o2.pl>
2022-01-06 19:10:00 +0100

    add hotkeys - open edit visual base
    Useful if you don't have developer tab enabled,
    or when editing several files in a series

--- hotkeys_data.php
+++ hotkeys_data.php
@@ -33,6 +33,7 @@ return ( [
 		t('Jump to the Active Cell')=> t('%s + %s',[CTRL, BACKSPACE]),
 		t('Add or edit a cell') => t('%s + %s',[SHIFT, t('F2')]), 
 		t('Change the cell format') => t('%s + %s + ( %s )',[CTRL, SHIFT, t('numeric 1-6 or `')]),
+		t('Open edit visual basic') => t('%s + %s',[ALT, 'F11']),
     ],
     t('PhpStrom') => [
         t('Autotext') => t('%s + %s', [CTRL, SPACE]),
