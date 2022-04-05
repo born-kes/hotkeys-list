dbdfcd0286c988a0f6c591852ebce8741f2a26d7
bornkes <born-kes@o2.pl>
2022-04-05 20:39:24 +0200

    style

--- hotkeys_data.php
+++ hotkeys_data.php
@@ -1,52 +1,54 @@
 <?php
 if(!function_exists('t')){ function t($a){return $a;} }
-define('ALT', t('Alt') );
-define('CTRL', t('Ctrl') );
-define('SHIFT', t('Shift') );
-define('SPACE', t('Space') );
-define('ENTER', t('Enter') );
-define('WIN', t('Window-key') );
-define('DOWN_ARROW', t('↓') );
-define('UPWARDS_ARROW', t('↑') );
-define('LEFTWARDS_ARROW', t('←') );
-define('RIGHTWARDS_ARROW', t('→') );
-define('BACKSPACE', t('Backspace') );
+
+define('ALT', t('Alt'));
+define('CTRL', t('Ctrl'));
+define('SHIFT', t('Shift'));
+define('SPACE', t('Space'));
+define('ENTER', t('Enter'));
+define('WIN', t('Window-key'));
+define('DOWN_ARROW', t('↓'));
+define('UPWARDS_ARROW', t('↑'));
+define('LEFTWARDS_ARROW', t('←'));
+define('RIGHTWARDS_ARROW', t('→'));
+define('BACKSPACE', t('Backspace'));
 
 return ( [
-	t('System') => [
-		t('Unlogging from windows') => t('%s + %s' ,[ WIN, t('L')]),
-		t('Hidden all windows, show desktop') => t('%s + %s', [WIN, t('d')]), 
-	],
-    t('all editor text') => [
-		t('Select all words') => t('%s + %s', [CTRL, t('a')]),
-		t('Undo the last change') => t('%s + %s', [CTRL, t('z') ]), 
-		t('Undo the last back change') => t('%s + %s', [CTRL, t('y') ]), 
-        t('select a word') => t('Ctrl + Shift + ->'),
-        t('Delete a word') => t('%s + $s', [CTRL, BACKSPACE]),
-        t('jump a word') => t('Ctrl + ->'),
-    ],
-    t('excel') => [
-        t('Add Filter') => t('Ctrl + Shift + L'),
-		t('Save as') => t('F12'),
-		t('Select all to the first blank')=> t('%s + %s + %s', [CTRL, SHIFT, DOWN_ARROW]),
-		t('Insert tables')=> t('%s + %s',[CTRL, t('T')]),
-		t('Jump to the Active Cell')=> t('%s + %s',[CTRL, BACKSPACE]),
-		t('Add or edit a cell') => t('%s + %s',[SHIFT, t('F2')]), 
-		t('Change the cell format') => t('%s + %s + ( %s )',[CTRL, SHIFT, t('numeric 1-6 or `')]),
-    ],
-    t('PhpStrom') => [
-        t('Autotext') => t('%s + %s', [CTRL, SPACE]),
-        t('Reformat code') => t('Ctrl + Alt + L'),
-		t('Show suggested or options auto-suggestions ') => t('%s + %s', [ALT, ENTER]),
-    ],
-    t('Netbeans') => [
-        t('Reformat code') => t('Alt + Shift + F'),
-        t('Open / Navigate to declaration') => t('%s + %s', [CTRL, 'B']),
-		t('Search excerpt text') => t('%s + %s',[CTRL, t('F3')]),
-		t('Search class and open edit') => t('%s + %s',[CTRL, t('o')]),
-		t('Back to last edit place') => t('%s + %s', [CTRL, 'Q']),
-		t('Add ; to the end of the line') => t('%s + %s', [CTRL, t(';')]),
-		t('Add ; to the end of the line and enter to new line') => t('%s + %s + %s', [CTRL, SHIFT, t(';')]),
-		t('Show suggested or options auto-suggestions ') => t('%s + %s', [ALT, ENTER]),
-    ],
-]);
+  t('System') => [
+    t('xx') => t('%s + %s', [LEFTWARDS_ARROW, RIGHTWARDS_ARROW]),
+    t('Unlogging from windows') => t('%s + %s', [WIN, t('L')]),
+    t('Hidden all windows, show desktop') => t('%s + %s', [WIN, t('d')]),
+  ],
+  t('all editor text') => [
+    t('Select all words') => t('%s + %s', [CTRL, t('a')]),
+    t('Undo the last change') => t('%s + %s', [CTRL, t('z')]),
+    t('Undo the last back change') => t('%s + %s', [CTRL, t('y')]),
+    t('select a word') => t('Ctrl + Shift + ->'),
+    t('Delete a word') => t('%s + $s', [CTRL, BACKSPACE]),
+    t('jump a word') => t('Ctrl + ->'),
+  ],
+  t('excel') => [
+    t('Add Filter') => t('Ctrl + Shift + L'),
+    t('Save as') => t('F12'),
+    t('Select all to the first blank') => t('%s + %s + %s', [CTRL, SHIFT, DOWN_ARROW]),
+    t('Insert tables') => t('%s + %s', [CTRL, t('T')]),
+    t('Jump to the Active Cell') => t('%s + %s', [CTRL, BACKSPACE]),
+    t('Add or edit a cell') => t('%s + %s', [SHIFT, t('F2')]),
+    t('Change the cell format') => t('%s + %s + ( %s )', [CTRL, SHIFT, t('numeric 1-6 or `')]),
+  ],
+  t('PhpStrom') => [
+    t('Autotext') => t('%s + %s', [CTRL, SPACE]),
+    t('Reformat code') => t('Ctrl + Alt + L'),
+    t('Show suggested or options auto-suggestions ') => t('%s + %s', [ALT, ENTER]),
+  ],
+  t('Netbeans') => [
+    t('Reformat code') => t('Alt + Shift + F'),
+    t('Open / Navigate to declaration') => t('%s + %s', [CTRL, 'B']),
+    t('Search excerpt text') => t('%s + %s', [CTRL, t('F3')]),
+    t('Search class and open edit') => t('%s + %s', [CTRL, t('o')]),
+    t('Back to last edit place') => t('%s + %s', [CTRL, 'Q']),
+    t('Add ; to the end of the line') => t('%s + %s', [CTRL, t(';')]),
+    t('Add ; to the end of the line and enter to new line') => t('%s + %s + %s', [CTRL, SHIFT, t(';')]),
+    t('Show suggested or options auto-suggestions ') => t('%s + %s', [ALT, ENTER]),
+  ],
+  ]);
